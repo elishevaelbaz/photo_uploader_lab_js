350dc200d6511d95991904e736a97b25a155cba9
github username <github email address>
2020-06-30 15:20:18 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -16,4 +16,64 @@ function openNewPicForm(){
 }
 
 //****Start coding below****//
+// DOM elements
+const photoContainer = document.querySelector("#photoContainer")
+const newPicForm = document.querySelector("#newPicForm")
+console.log(newPicForm)
+// Event listeners
 
+// add new photo 
+newPicForm.addEventListener("submit", function(e){
+    e.preventDefault()
+
+    fetch("http://localhost:3000/photos", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: e.target.name.value,
+            photo_image_url: e.target.photo_image_url.value,
+            owner: e.target.owner.value
+        }),
+    })
+    .then(r => r.json())
+    .then(renderOnePhoto)
+})
+
+function deletePhoto(photoObj){
+      
+        fetch(`http://localhost:3000/photos/${photoObj.id}`, {
+            method: 'DELETE'
+        })
+        .then(r => r.json())
+        .then(console.log)
+}
+
+// Render helpers
+function renderOnePhoto(photoObj){
+    const photoDiv = document.createElement("div")
+    photoDiv.className = "photo"
+    photoDiv.innerHTML += `<h3>${photoObj.name}</h3>
+    <p>By ${photoObj.owner}</p>
+    <img src="${photoObj.photo_image_url}">
+    <button class="removeButton" data-id="${photoObj.id}">Remove</button>`
+
+    photoContainer.append(photoDiv)
+    const removeButton = photoDiv.querySelector(".removeButton")
+    // event listener to delete
+    removeButton.addEventListener("click", function(e){
+        photoDiv.remove()
+
+        // optimistic rendering
+        deletePhoto(photoObj)
+    })
+}
+ // initialize
+
+ fetch("http://localhost:3000/photos")
+ .then(r => r.json())
+ .then(photosArray => {
+    photosArray.forEach(renderOnePhoto)
+ })
